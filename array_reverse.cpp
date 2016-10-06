6924be448d95fb60d61c5fefc72b8753777e96ec
Ayush kumar bharti <aayushkumar1011@gmail.com>
2016-10-06 23:59:38 +0530

    Update array_reverse.cpp

--- array_reverse.cpp
+++ array_reverse.cpp
@@ -4,10 +4,10 @@ void reverse(int arr[], int n, int k){
 	
 	for (int i = 0; i <n; i+=k)
 	{
-  int left= i;
-  int right = min(i+k-1, n-1);
-  while(left < right){
-    swap(arr[left++], arr[right--]);
+	  int left= i;
+	  int right = min(i+k-1, n-1);
+	  while(left < right){
+	    swap(arr[left++], arr[right--]);
 
 		}
 	}
